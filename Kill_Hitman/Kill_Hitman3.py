5a36d9069bdb41177e82e2d29147c4db0b33d75e
justbest23 <73498310+justbest23@users.noreply.github.com>
2023-07-14 11:41:25 +0200

    Update Kill_Hitman3.py
    Added variables for game and hotkey selection.

--- Kill_Hitman/Kill_Hitman3.py
+++ Kill_Hitman/Kill_Hitman3.py
@@ -11,11 +11,14 @@ import winreg
 ## Use the variable to change the hotkey. Do this BEFORE running the "install_the_things.bat" file! ##
 hotkey = 'ctrl+q'
 
-def kill_hitman3():
+## Use the variable to change the game you want to kill. Do this BEFORE running the "install_the_things.bat file! ##
+game = 'Hitman3.exe'
+
+def kill_game():
     # Get all running processes
     for proc in psutil.process_iter(['pid', 'name']):
         # Check if the process is Hitman3.exe
-        if proc.info['name'] == 'Hitman3.exe':
+        if proc.info['name'] == game:
             # Terminate the process
             proc.kill()
 
@@ -34,30 +37,9 @@ def setup_system_tray():
     # Run the system tray
     tray_icon.run()
 
-def add_to_startup():
-    # Get the current user's Startup folder path
-    startup_folder = os.path.join(os.getenv('ProgramData'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-
-    # Get the path to the script's executable
-    script_exe = os.path.abspath(sys.argv[0])
-
-    # Create the destination shortcut path
-    shortcut_path = os.path.join(startup_folder, 'Kill_Hitman3.lnk')
-
-    # Create a shortcut
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shortcut = shell.CreateShortCut(shortcut_path)
-    shortcut.TargetPath = script_exe
-    shortcut.WorkingDirectory = os.getcwd()
-    shortcut.IconLocation = os.path.join(os.getcwd(), 'Fancy-Logo.jpg')
-    shortcut.Save()
-
 if __name__ == '__main__':
-    keyboard.add_hotkey({hotkey}, kill_hitman3)
+    keyboard.add_hotkey(hotkey, kill_game)
     setup_system_tray()
 
     # Copy the "Fancy-Logo.jpg" file to the executable directory
     shutil.copy2('Fancy-Logo.jpg', sys.argv[0])
-
-    # Add to Windows Startup
-    add_to_startup()
